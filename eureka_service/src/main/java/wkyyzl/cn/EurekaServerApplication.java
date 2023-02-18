fdc698078c7a55674458c7da0bb80240d1470a18
wukongyong <1334306978@qq.com>
2023-02-18 08:41:34 +0800

    add eureka_server

---
+++ eureka_service/src/main/java/wkyyzl/cn/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package wkyyzl.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//激活eurekaServer
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
